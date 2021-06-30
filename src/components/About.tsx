cfa5bd9357dceff763bcbdead7822424555146fa
Tudor Alexandru <tudorale2005@gmail.com>
2021-06-30 08:25:57 +0300

    initiating about files

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './subcomponent/Navbar'
+import "../style/css/main.css";
+import Footer from './subcomponent/Footer';
+
+function About() {
+    return (
+        <>
+            <Navbar /> 
+                <div className="about">
+                    
+                </div>
+            <Footer /> 
+        </>
+    )
+}
+
+export default About
